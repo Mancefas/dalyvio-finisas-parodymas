a2a458c02a5a2924dc43f8989328f2e08906b4ef
Mancefas <mantvydasliobe@gmail.com>
2021-10-21 20:33:09 +0300

    error handling, bug with global naming

--- src/Components/RezultataiPrel.js
+++ src/Components/RezultataiPrel.js
@@ -6,26 +6,29 @@ const RezultataiPrel = () => {
   const [rezultatai, setRezultatai] = useState([]);
   const [loading, setLoading] = useState();
   const [show, setShow] = useState(false);
+  const [error, setError] = useState(null);
 
   const rezultataiDataHandler = async () => {
     setLoading(true);
-    const response = await fetch(
-      "https://gif-rezultatai-b73a6-default-rtdb.europe-west1.firebasedatabase.app/rez.json"
-    );
-    const data = await response.json();
-    const dataGauta = [];
+    try {
+      const response = await fetch(
+        "https://gif-rezultatai-b73a6-default-rtdb.europe-west1.firebasedatabase.app/rez.json"
+      );
+      const data = await response.json();
+      const dataGauta = [];
 
-    for (const key in data) {
-      dataGauta.push({
-        id: key,
-        atvaziavo: data[key].atvaziavo,
-        laikas: data[key].laikas,
-        dalyvis: data[key].dalyvis,
-      });
+      for (const key in data) {
+        dataGauta.push({
+          id: key,
+          atvaziavo: data[key].atvaziavo,
+          laikas: data[key].laikas,
+          dalyvis: data[key].dalyvis,
+        });
+      }
+      setRezultatai(dataGauta);
+    } catch (error) {
+      setError(error.message);
     }
-    console.log(data);
-    console.log(dataGauta);
-    setRezultatai(dataGauta);
     setLoading(false);
     setShow(!show);
   };
@@ -34,16 +37,18 @@ const RezultataiPrel = () => {
     <Container>
       {loading && <LinearProgress color="success" />}
 
+      {error && <p className={"errMsg"}>Error : {error}</p>}
+
       <Container className={"btn"} maxWidth="xs">
-        <Button onClick={rezultataiDataHandler} variant="outlined">
+        <Button onClick={rezultataiDataHandler} variant="outlined" disabled>
           Rezultatai-preliminarūs
         </Button>
       </Container>
       {show && (
         <Container className={"container"}>
-          <ul>
+          <ul className={"ul"}>
             {rezultatai.map((rezultatas, indx) => (
-              <li key={rezultatas.laikas}>
+              <li className={"li"} key={rezultatas.laikas}>
                 <Paper elevation={4}>
                   <h2>Dalyvio numeris : {rezultatas.dalyvis}</h2>
                   <h3>Atvažiavo į finišą : {indx + 1}</h3>
