403f00946c7334a232b76ae8bcf524c0196d68e9
Mancefas <mantvydasliobe@gmail.com>
2021-10-27 09:56:54 +0300

    center text in header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
             className={classes.bike2}
           />
         </div>
-        <h3 className={classes.giraText}>KAZLŲ RŪDOS GIRIA</h3>
+        <h3 className={classes.giriaText}>KAZLŲ RŪDOS GIRIA</h3>
         <h4 className={classes.kmText}>80KM 130KM</h4>
       </Container>
     </header>
