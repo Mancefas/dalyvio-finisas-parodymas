1c0f3c093535e8eaba529f5d56442a54991d1fcd
Mancefas <mantvydasliobe@gmail.com>
2021-10-27 09:33:12 +0300

    added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 import RezultataiGal from "./Components/RezultataiGal/RezultataiGal";
 import RezultataiPrel from "./Components/RezultataiPrel";
 
 function App() {
   return (
     <>
+      <Header />
       <RezultataiPrel />
       <RezultataiGal />
     </>
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,31 @@
+import classes from "./Header.module.css";
+import React from "react";
+import { Container } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBiking, faBicycle } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+  return (
+    <header>
+      <Container maxWidth="md">
+        <div className={classes.header}>
+          <FontAwesomeIcon
+            icon={faBiking}
+            size={"2x"}
+            className={classes.bike1}
+          />
+          <h1>Gravel investicijų fondas</h1>
+          <FontAwesomeIcon
+            icon={faBicycle}
+            size={"2x"}
+            className={classes.bike2}
+          />
+        </div>
+        <h3 className={classes.giraText}>KAZLŲ RŪDOS GIRIA</h3>
+        <h4 className={classes.kmText}>80KM 130KM</h4>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
