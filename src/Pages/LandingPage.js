ba89c661e12ed9e847e507bb9194b59bc15c9e3b
Mancefas <mantvydasliobe@gmail.com>
2021-11-25 10:56:24 +0200

    Landing page

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,17 +1,20 @@
 import React from "react";
+
 import { NavLink } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/gif-kazlu-ruda">GIF Kazlų Rūda</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/gif-kazlu-ruda">GIF Kazlų Rūda</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 };
 
