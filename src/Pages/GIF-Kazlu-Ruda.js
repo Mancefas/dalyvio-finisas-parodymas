8ee1e84eed29bf5a1e06dedb8563d442f1222a80
Mancefas <mantvydasliobe@gmail.com>
2021-11-29 19:56:05 +0200

    min height for component added

--- src/Pages/GIF-Kazlu-Ruda.js
+++ src/Pages/GIF-Kazlu-Ruda.js
@@ -3,14 +3,15 @@ import React from "react";
 import RaceHeader from "../Components/RaceHeader/RaceHeader";
 import RezultataiPrel from "../Components/Rezultatai/Rezultatai-preliminarus/RezultataiPrel";
 import RezultataiApdoroti from "../Components/Rezultatai/Rezultatai-is-suvestu/RezultataiApdoroti";
+import { Container } from "@mui/material";
 
 const GifKazluRuda = () => {
   return (
-    <>
+    <Container sx={{ minHeight: "72vh" }}>
       <RaceHeader />
       <RezultataiPrel />
       <RezultataiApdoroti />
-    </>
+    </Container>
   );
 };
 
