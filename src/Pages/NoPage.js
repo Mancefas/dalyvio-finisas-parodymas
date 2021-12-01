681156b44a23a17b9531928346f8e858a021bef2
Mancefas <mantvydasliobe@gmail.com>
2021-12-01 21:04:50 +0200

    translation to ENG 404 page

--- src/Pages/NoPage.js
+++ src/Pages/NoPage.js
@@ -1,9 +1,13 @@
+import React, { useContext } from "react";
 import { Container, Box, Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGhost } from "@fortawesome/free-solid-svg-icons";
 
+import Context from "../store/Context";
+
 const NoPage = () => {
+  const context = useContext(Context);
   return (
     <Container sx={{ minHeight: "72vh" }}>
       <Box>
@@ -11,13 +15,21 @@ const NoPage = () => {
           404
         </Typography>
         <FontAwesomeIcon icon={faGhost} size="3x" color="gray" />
-        <Typography variant="h4">Ups pasiklydom...</Typography>
-        <Typography variant="overline">Tokio puslapio nėra</Typography>
+        <Typography variant="h4">
+          {context.language === "ENG"
+            ? "Ohh we are lost..."
+            : "Ups pasiklydom..."}{" "}
+        </Typography>
+        <Typography variant="overline">
+          {context.language === "ENG"
+            ? "There is no page with this name"
+            : "Tokio puslapio nėra"}{" "}
+        </Typography>
         <Box sx={{ display: "flex", flexDirection: "column" }}>
           <Box m="1rem">
             {/* Reikia isivesti email kai bus susisiekti!!! */}
             <Button variant="outlined" href="mailto:">
-              Susisiekti
+              {context.language === "ENG" ? "Contect us" : "Susisiekti"}
             </Button>
           </Box>
           <Link
@@ -26,7 +38,10 @@ const NoPage = () => {
               textDecoration: "none",
             }}
           >
-            <Button variant="contained">Į pradžią</Button>{" "}
+            <Button variant="contained">
+              {" "}
+              {context.language === "ENG" ? "Go back" : "Į pradžią"}
+            </Button>{" "}
           </Link>
         </Box>
       </Box>
