90ff7482cb15c24fdbc0386ce23a414ec658a467
Mancefas <mantvydasliobe@gmail.com>
2022-04-07 21:04:30 +0300

    different landing page component mobile/dpc

--- src/Components/Graveltonas/RaceHeaderGravelonas.js
+++ src/Components/Graveltonas/RaceHeaderGravelonas.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
 import graveltonas from "../../img/graveltonas.jpg";
 import classes from "./RaceHeaderGravelonas.module.css";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+
+import Context from "../../store/Context";
 
 const RaceHeaderGraveltonas = () => {
+  const context = useContext(Context);
+
   return (
     <>
       <Container
         maxWidth="md"
-        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+        sx={{
+          alignItems: "center",
+          justifyContent: "center",
+          display: { xs: "none", md: "flex", lg: "flex" },
+        }}
       >
         <img
           src={graveltonas}
           alt="gravelonas varzybos"
           className={classes.img}
         />
+      </Container>
+      <Container
+        maxWidth="md"
+        sx={{
+          display: { xs: "flex", md: "none", lg: "none" },
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          margin: "auto",
+          backgroundColor: "#FF6634",
+          paddingTop: "2rem",
+          paddingBottom: "2rem",
+        }}
+      >
+        <Typography variant="h5">
+          {context.language === "ENG" ? "April 9" : "Baladžio 9"}
+        </Typography>
+        <Typography variant="h3" sx={{ fontWeight: "bold" }}>
+          Gravel TT
+        </Typography>
+        <Typography variant="h3" sx={{ fontWeight: "bold" }}>
+          #Gravelonas
+        </Typography>
+        <Typography variant="h5">Kaunas</Typography>
       </Container>
     </>
   );
