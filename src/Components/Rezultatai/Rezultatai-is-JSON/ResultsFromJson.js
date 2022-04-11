0c8778a9bb5dbf03238920d6d959dd67ad212bb0
Mancefas <mantvydasliobe@gmail.com>
2022-04-11 21:16:23 +0300

    Component to get data from JSON(not server)

---
+++ src/Components/Rezultatai/Rezultatai-is-JSON/ResultsFromJson.js
@@ -0,0 +1,49 @@
+import React, { useState, useContext, useEffect } from "react";
+import { Button, Container, LinearProgress, Alert } from "@mui/material";
+import RezultataiRodymasTT from "../RezultataiRodymasTT";
+
+import Context from "../../../store/Context";
+
+const ResultsFromJson = (props) => {
+  const context = useContext(Context);
+  // Getting links to call APIs from props
+  const participantsWithData = props.data;
+
+  const [rezultataiSuInfo, setRezultataiSuInfo] = useState([]);
+
+  const dataFromAPIHandler = (props) => {
+    const data = participantsWithData;
+    const dataDalyviu = data;
+    const dalyviuArray = [];
+
+    for (const key in dataDalyviu) {
+      dalyviuArray.push({
+        distancija: dataDalyviu[key].dist,
+        dviratis: dataDalyviu[key].dviratis,
+        kategorija: dataDalyviu[key].grupe,
+        laikas: dataDalyviu[key].vaziavimoLaikas,
+        laikasMS: +dataDalyviu[key].vaziavimoLaikasMS,
+        numeris: dataDalyviu[key].startoNr.toString(),
+        vardas: dataDalyviu[key].vardas[0],
+        pavarde: dataDalyviu[key].vardas[1],
+      });
+    }
+
+    setRezultataiSuInfo(dalyviuArray);
+  };
+
+  useEffect(() => {
+    dataFromAPIHandler();
+  }, []);
+
+  return (
+    <Container>
+      <Button variant="contained" sx={{ marginTop: "2rem" }}>
+        {context.language === "ENG" ? "🏆 Results 🏆" : "🏆 Rezultatai 🏆"}{" "}
+      </Button>
+      {<RezultataiRodymasTT galutiniaiRez={rezultataiSuInfo} />}
+    </Container>
+  );
+};
+
+export default ResultsFromJson;
--- src/Pages/Gravelonas.js
+++ src/Pages/Gravelonas.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Container } from "@mui/material";
 import RaceHeaderGravelonas from "../Components/Graveltonas/RaceHeaderGravelonas";
-import RezultataiApdorotiTT from "../Components/Rezultatai/Rezultatai-is-suvestu/RezultataiApdorotiTT";
+// import RezultataiApdorotiTT from "../Components/Rezultatai/Rezultatai-is-suvestu/RezultataiApdorotiTT";
+import ResultsFromJson from "../Components/Rezultatai/Rezultatai-is-JSON/ResultsFromJson";
 
-import config from "../config.json";
+// import config from "../config.json";
+import dataFromJson from "../Race-results-jsons/gravelonas-default-rtdb-export.json";
 
 const Gravelonas = () => {
-  const dataFromAPI = config.API_URL_Gravelonas_data;
+  // getting data from json file (downloaded from server)
+  const dataFromAPI = dataFromJson["dal"];
+  // getting data from server
+  // const dataFromAPI = config.API_URL_Gravelonas_data;
 
   return (
     <>
       <Container sx={{ minHeight: "72vh", marginBottom: "0.5rem" }}>
         <RaceHeaderGravelonas />
         <Container maxWidth="xl">
-          <RezultataiApdorotiTT data={dataFromAPI} />
+          {/* Shows data from server */}
+          {/* <RezultataiApdorotiTT data={dataFromAPI} /> */}
+          {/* Getting data from JSON(downloaded from server) */}
+          <ResultsFromJson data={dataFromAPI} />
         </Container>
       </Container>
     </>
